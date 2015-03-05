79c0cadca3291d54843c6718804216aac06d7e06
Gabe Kangas <gabe.kangas@rd.io>
2015-03-05 15:43:14 -0800

    Added a timestamp for the time the song object was created.  But that is not terribly helpful for an old queue

--- RdioParty/Song.swift
+++ RdioParty/Song.swift
@@ -3,7 +3,6 @@
 //  RdioParty
 //
 //  Created by Gabe Kangas on 2/28/15.
-//  Copyright (c) 2015 Rdio. All rights reserved.
 //
 
 import UIKit
@@ -21,6 +20,7 @@ class Song: NSObject {
     var artistName :String!
     var trackName :String!
     var color :UIColor!
+    var timestampAdded :NSDate!
     var queued = true
     
     
@@ -29,6 +29,7 @@ class Song: NSObject {
     }
     
     init(fromSnapshot snapshot :FDataSnapshot) {
+        self.timestampAdded = NSDate()
         self.trackKey = snapshot.value.valueForKey("trackKey") as! String
         self.partyId = snapshot.key
         self.userKey = snapshot.value.valueForKey("userKey") as! String
