792f0e333c9990958990e5e69531b9045dfe562f
Gabe Kangas <gabe.kangas@rd.io>
2015-03-10 15:32:04 -0700

    Centralizing credentials

---
+++ RdioParty/Credentials.swift
@@ -0,0 +1,16 @@
+//
+//  Credentials.swift
+//  RdioParty
+//
+//  Created by Gabe Kangas on 3/10/15.
+//
+
+import UIKit
+
+struct Credentials {
+    static let RdioConsumerKey = "ADDKEY"
+    static let RdioConsumerSecret = "ADDSECRET"
+    
+    static let RdioPartyFirebaseToken = "ADDTOKEN"
+    
+}
