15e3cb525d4ca1c906ca7b7f248c12794097886a
Gabe Kangas <gabek@real-ity.com>
2015-03-14 14:53:27 -0700

    Added queue voting.  Different than player voting.

--- RdioParty/PartyPlayerManager.swift
+++ RdioParty/PartyPlayerManager.swift
@@ -9,32 +9,39 @@ import UIKit
 
 class PartyPlayerManager: NSObject {
    
-    var firebaseRef :Firebase!
+    var firebase :Firebase!
+    var rdio :Rdio!
     let userid = UIApplication.rdioPartyApp.session.user.rdioId
     
     convenience init(firebaseRef ref :Firebase) {
         self.init()
-        self.firebaseRef = ref
+        self.firebase = ref
     }
     
     func addTrackToQueue(trackKey:String) {
         var track = ["trackKey": trackKey, "userKey" : userid, "votes" : [userid : "like"]]
-        var postRef = self.firebaseRef.childByAutoId()
+        var postRef = firebase.childByAutoId()
         postRef.setValue(track)
     }
-    
+
     func voteDownSong(song :Song) {
-        
+        let postRef = firebase.childByAutoId()
+        let post = ["trackKey": song.trackKey, "userKey": userid, "votes" : [userid : "dislike"]]
+        postRef.setValue(post)
     }
     
     func voteUpSong(song :Song) {
-        
+        let postRef = firebase.childByAutoId()
+        let post = ["trackKey": song.trackKey, "userKey": userid, "votes" : [userid : "like"]]
+        postRef.setValue(post)
     }
     
+    // Need to find the actual syntax of the object to send for this.
     func skipCurrentSong(song :Song) {
-        
+
     }
     
+    // Add to user's Rdio favorites
     func favoriteSong(song :Song) {
         
     }
